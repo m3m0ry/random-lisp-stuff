05bd6976f71dff31033333ec55652cf2927b1628
Jan Hönig <jan.hoenig@fau.de>
2019-06-01 16:55:23 +0200

    Implementation of feed forward and backpropagation should be done TODO: Gradient descent, input/output, epochs

--- neuwerk/base.lisp
+++ neuwerk/base.lisp
@@ -9,21 +9,17 @@
   (* (logistic-function x) (- 1 (logistic-function x))))
 
 
-
-
-
-
 ;; Petalisp functions
 (defun delta-last (a z target &optional (func-deriv 'logistic-function-deriv))
   (α #'*
      (α #'- a target)
      (α func-deriv z)))
 
-(defun delta (w-prev z delta-prev &optional (func-deriv 'logistic-function-deriv))
+(defun delta (w-next z delta-next &optional (func-deriv 'logistic-function-deriv))
   (α #'*
      (α #'*
-        (reshape w-prev (τ (i j) (j i)))
-        delta-prev)
+        (reshape w-next (τ (i j) (j i)))
+        delta-next)
      (α func-deriv z)))
 
 (defun delta-b (d)
@@ -43,7 +39,7 @@
 (defun vec-add (v1 v2)
   (α #'+ v1 v2))
 
-(defun neuron-input (W a b)
+(defun neuron-input (W b a)
   (vec-add
    (mat-vec-mul W a)
    b))
@@ -51,15 +47,36 @@
 (defun neuron-output (z &optional (func 'logistic-function))
   (α func z))
 
+(defun feedforward-step (W b a &optional (func 'logistic-function))
+  (let ((z (neuron-input W b a)))
+    (values (neuron-output z func) z)))
+
+(defun feedforward (weights biases input &optional (func 'logistic-function))
+  (let ((as '()) (zs '()))
+    (loop for W in weights
+          for b in biases
+          do (multiple-value-bind (input z) (feedforward-step W b input func)
+               (nconc input as)
+               (nconc z zs)))
+    (values as zs)))
+
+(defun backpropagation (weights biases x y)
+  (multiple-value-bind (as zs) (feedforward weights biases x)
+    (let ((ds (delta-last (last as) (last zs) y))
+          (dw '()) (db '()))
+      (loop for W in (reverse weights)
+            for b in (reverse biases)
+            for a in (reverse (cons x as))
+            for z in (cdr (reverse zs))
+            do (push (delta W z (first ds)) ds))
+      (loop for a in (cons x as)
+            for d in ds
+            collect (delta-weight a d) into dw
+            collect (delta-b d) into db)
+      (values dw db))))
+
 (defun cost (a target)
   (α #'/
      (β #'+
         (α 'expt (α #'- target a) 2))
      2))
-
-;;(defun mat-vec-mul2 (A v)
-;;  (β #'+
-;;     (reshape
-;;      (α #'* A
-;;         (reshape v (τ (i) (0 i))))
-;;      (τ (i j) (j i)))))
