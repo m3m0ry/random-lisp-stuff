e90a4eb1370b1d55e155deca3110213d2b3fba7c
Jan Hönig <jan.hoenig@fau.de>
2019-06-02 21:50:17 +0200

    there is a bug

--- neuwerk/base.lisp
+++ neuwerk/base.lisp
@@ -25,7 +25,7 @@
 (defun delta-b (d)
   d)
 
-(defun delta-weight (a-prev d)
+(defun delta-weight (a-prev d) ;; TODO check this
   (α #'*
      (reshape a-prev (τ (i) (i 0)))
      (reshape d (τ (i) (0 i)))))
@@ -56,24 +56,46 @@
     (loop for W in weights
           for b in biases
           do (multiple-value-bind (input z) (feedforward-step W b input func)
-               (nconc input as)
-               (nconc z zs)))
+               (push input as)
+               (push z zs)))
     (values as zs)))
 
 (defun backpropagation (weights biases x y)
   (multiple-value-bind (as zs) (feedforward weights biases x)
-    (let ((ds (delta-last (last as) (last zs) y))
+    (let ((ds (list (delta-last (last as) (last zs) y)))
           (dw '()) (db '()))
       (loop for W in (reverse weights)
             for b in (reverse biases)
-            for a in (reverse (cons x as))
-            for z in (cdr (reverse zs))
+            for a in (reverse (cons x as)) ;;TODO drop last?
+            for z in (cdr (reverse zs)) ;;TODO with droping i might have one too little iterations
             do (push (delta W z (first ds)) ds))
+      (format t "~a~%" ds)
+      (format t "~a~%" as)
       (loop for a in (cons x as)
             for d in ds
             collect (delta-weight a d) into dw
             collect (delta-b d) into db)
       (values dw db))))
+
+(defun gradient-descent (values grad eta)
+  (α #'- values (α #'* eta grad)))
+
+(defun learn (weights biases func eta)
+  (loop for r to 1000
+        for x = (random 1.0)
+        do (multiple-value-bind
+                 (dws dbs)
+               (backpropagation weights biases (list x x) (list (funcall func x) (funcall func x)))
+             (loop for dw in dws
+                   for W in weights
+                   for db in dbs
+                   for b in biases
+                   collect (compute (gradient-descent W dw eta)) into new-weights
+                   collect (compute (gradient-descent b db eta)) into new-biases
+                   finally (setf weights new-weights)
+                   finally (setf biases new-biases)))
+        do (format t "Step ~a~%" r))
+  (values weights biases))
 
 (defun cost (a target)
   (α #'/
